eaba0e5dc80781e4f9aa5dfbc800667141fe2095
Klaven Jones <klavensjones@gmail.com>
2021-09-27 01:53:22 -0400

    A few minor chages

--- client/src/styles/App.js
+++ client/src/styles/App.js
@@ -3,8 +3,19 @@ import { makeStyles } from "@material-ui/core";
 export const useHomeStyles = makeStyles((theme) => ({
   root: {
     height: "100vh"
+  },
+  logout: {
+    textTransform: "none",
+    letterSpacing: 0,
+    margin: "40px 0 60px 20px",
+    fontWeight: "bold",
+    fontSize: "18px",
+    height: "56px",
+    width: "170px",
+    boxShadow: "0 0 8px 0 rgba(0 0 0 / 20%)"
   }
 }));
+
 export const useErrorStyles = makeStyles((theme) => ({
   snackbar: {
     backgroundColor: "red",
